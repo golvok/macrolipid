760f48d717668dc63a9a631d453782962c6e1b8e
Matthew J. P. Walker <walkerm930@gmail.com>
2020-06-28 11:27:03 -0400

    init more like a bilayer

--- src/initialization.rs
+++ src/initialization.rs
@@ -1,17 +1,26 @@
 use crate::types::*;
+use cgmath::Basis2;
+use cgmath::Rad;
+use cgmath::Rotation;
+use cgmath::Rotation2;
 
 pub fn default() -> State {
     let mut result = State { lipids: vec![] };
-    for i in 0..1000 {
-        let x = (i % 100) as f32;
-        let y = (i / 100) as f32;
-        let flip = if i % 2 == 0 { -1. } else { 1. };
-        result.lipids.push(Lipid {
-            head_position: (100.0 + 6.0 * x, 100.0 + 6.0 * y).into(),
-            tail_position: (100.0 + flip * 5.0 + 6.0 * x, 100.0 + flip * 5.0 + 6.0 * y).into(),
-            head_radius: 2.,
-            tail_length: 10.,
-        })
+    for irow in 0..=1 {
+        for icol in 0..400 {
+            let centre = Point::new(100.0 + icol as f32 * 1.0, 100.0 + irow as f32 * 10.0);
+            let tail_length = 10.0;
+            let tail_vec_x = Vector::new(tail_length * 0.5, 0.0);
+            let angle = if irow % 2 == 0 { 0.5 } else { 1.5 };
+            let rot: Basis2<f32> = Rotation2::from_angle(Rad(angle * std::f32::consts::PI));
+            let offset_vec = rot.rotate_vector(tail_vec_x);
+            result.lipids.push(Lipid {
+                head_position: centre - offset_vec,
+                tail_position: centre + offset_vec,
+                head_radius: 3.,
+                tail_length: tail_length,
+            })
+        }
     }
 
     result
