945143a51f0845a3e7b5ae6b7609d8d8c2ef99db
Matthew J. P. Walker <walkerm930@gmail.com>
2025-07-29 01:47:00 -0400

    head<->tail forces -- forms micells (monolayer cirles)

--- src/initialization.rs
+++ src/initialization.rs
@@ -11,11 +11,11 @@ pub fn default() -> State {
         tick_time: Duration::ZERO,
     };
     for irow in 0..=1 {
-        for icol in 0..200 {
-            let centre = Point::new(100.0 + icol as f32 * 1.0, 100.0 + irow as f32 * 10.0 + (icol % 5) as f32 * 2.0);
+        for icol in 0..20 {
+            let centre = Point::new(100.0 + icol as f32 * 3.5, 100.0 + irow as f32 * 20.0 + (icol % 3) as f32 * 5.25);
             let tail_length = 10.0;
             let tail_vec_x = Vector::new(tail_length * 0.5, 0.0);
-            let angle = if irow % 2 == 0 { 0.5 } else { 1.5 };
+            let angle = if irow % 2 == 0 { 1.0 } else { 0.0 };
             let rot: Basis2<f32> = Rotation2::from_angle(Rad(angle * std::f32::consts::PI));
             let offset_vec = rot.rotate_vector(tail_vec_x);
             result.lipids.push(Lipid {
